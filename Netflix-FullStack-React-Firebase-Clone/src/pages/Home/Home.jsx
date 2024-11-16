f1287999aac436a590d924b43ea3e8c04853e906
Bona Hirpa <bonaforg2@gmail.com>
2024-11-16 12:39:56 +0300

    Navbar added

--- Netflix-FullStack-React-Firebase-Clone/src/pages/Home/Home.jsx
+++ Netflix-FullStack-React-Firebase-Clone/src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import "./Home.css"
 import Navbar from "../../components/Navbar/Navbar"
 import hero_banner from "../../assets/cards/hero_banner.jpg"
 import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png"
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from '../../components/TitleCards/TitleCards';
 const Home = () => {
   return (
     <div className="home">
@@ -12,15 +15,23 @@ const Home = () => {
         <div className="hero-caption">
           <img src={hero_title} alt="" className="caption-img" />
           <p>
-            {" "}
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              cursus ipsum sapien, sed laoreet tortor ultricies quis. Sed sit
-              amet augue libero. Proin ut tellus luctus, auctor diam a,
-              pellentesque augue. Nunc aliquet ipsum nec tortor rhoncus, non
-              suscipit enim feugiat. Ut eleifend suscipit pulvinar.
-            </p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus
+            ipsum sapien, sed laoreet tortor ultricies quis. Sed sit amet augue
+            libero. Proin ut tellus luctus, auctor diam a, pellentesque augue.
+            Nunc aliquet ipsum nec tortor rhoncus, non suscipit enim feugiat. Ut
+            eleifend suscipit pulvinar.
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
         </div>
       </div>
     </div>
